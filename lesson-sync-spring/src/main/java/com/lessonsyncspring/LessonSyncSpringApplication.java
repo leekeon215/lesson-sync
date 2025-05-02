fe8de03565861756234c186707d4737d1e63f51f
leekeon215 <leekeon215@gmail.com>
2025-05-02 15:06:20 +0900

    initial spring boot server

---
+++ lesson-sync-spring/src/main/java/com/lessonsyncspring/LessonSyncSpringApplication.java
@@ -0,0 +1,13 @@
+package com.lessonsyncspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonSyncSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonSyncSpringApplication.class, args);
+    }
+
+}
---
+++ lesson-sync-spring/src/test/java/com/lessonsyncspring/LessonSyncSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lessonsyncspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonSyncSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
