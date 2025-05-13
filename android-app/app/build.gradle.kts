4f9f73452c0420a9402bca6d9497d1f353da3477
leekeon215 <leekeon215@gmail.com>
2025-05-13 16:06:06 +0900

    modify build files

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -21,27 +21,35 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
 }
 
 dependencies {
     // Compose BOM 사용
-    implementation(platform("androidx.compose:compose-bom:2024.03.00"))
+    implementation(platform(libs.androidx.compose.bom.v20240300))
 
     // AndroidX 코어/라이프사이클/Activity
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.1")
+    implementation(libs.androidx.core.ktx.v190)
+    implementation(libs.androidx.lifecycle.runtime.ktx.v262)
+    implementation(libs.androidx.activity.compose)
 
     // Compose UI
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation(libs.ui)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.material3)
 
     // (선택) 네비게이션 컴포즈
-    implementation("androidx.navigation:navigation-compose:2.7.0")
+    implementation(libs.androidx.navigation.compose)
 
-    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation(libs.dagger)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    debugImplementation(libs.ui.tooling)
 }
